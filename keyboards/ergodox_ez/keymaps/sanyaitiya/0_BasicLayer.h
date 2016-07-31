3713e5b98c8860f2b70dea4c6c691eef5845754f
Kazuya Yamamoto <fragment.vertex@gmail.com>
2016-07-31 13:56:12 +0900

    [add] Altキーを追加

--- keyboards/ergodox_ez/keymaps/sanyaitiya/0_BasicLayer.h
+++ keyboards/ergodox_ez/keymaps/sanyaitiya/0_BasicLayer.h
@@ -27,7 +27,7 @@
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   M(1),
-        KC_LGUI        ,KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+        KC_LGUI        ,KC_LALT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
